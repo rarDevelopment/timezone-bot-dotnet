497e682715406f46e0b24576348b9ffac8b5eec0
Robert R <rarDevelopment@users.noreply.github.com>
2022-12-22 22:43:40 -0500

    add version command

---
+++ TimeZoneBot/Commands/VersionCommand.cs
@@ -0,0 +1,25 @@
+﻿using DiscordDotNetUtilities.Interfaces;
+using TimeZoneBot.Models;
+
+namespace TimeZoneBot.Commands
+{
+    public class VersionCommand : InteractionModuleBase<SocketInteractionContext>
+    {
+        private readonly VersionSettings _versionSettings;
+        private readonly IDiscordFormatter _discordFormatter;
+
+        public VersionCommand(VersionSettings versionSettings, IDiscordFormatter discordFormatter)
+        {
+            _versionSettings = versionSettings;
+            _discordFormatter = discordFormatter;
+        }
+
+        [SlashCommand("version", "Get the current version number of the bot.")]
+        public async Task VersionSlashCommand()
+        {
+            await RespondAsync(embed: _discordFormatter.BuildRegularEmbed("Bot Version",
+                $"TimeZoneBot is at version **{_versionSettings.VersionNumber}**",
+                Context.User));
+        }
+    }
+}
